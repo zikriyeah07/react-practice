666d7041f70c3df1cd838b702ef132daa1a2ad55
Zikria W. <zikriyakhan64642@gmail.com>
2025-12-24 00:02:39 +0500

    add

---
+++ practice01/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Header from "./Header"
+import Footer from "./Footer"
+function App() {
+
+  return (
+     <>
+      <Header/> 
+      <Footer/>
+    </>
+     
+  )
+}
+
+export default App
---
+++ practice01/src/Footer.jsx
@@ -0,0 +1,9 @@
+
+function Footer() {
+    return (
+        <footer>
+            <p>© {new Date().getFullYear()} GENZIC</p>
+        </footer>
+    );
+}
+export default Footer;
