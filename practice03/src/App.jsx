6d91e8953c41f022fdc492f3c0766d0491ee23c6
Zikria W. <zikriyakhan64642@gmail.com>
2025-12-27 20:23:56 +0500

    props added, with learning of propstype and its installation also

--- practice03/src/App.jsx
+++ practice03/src/App.jsx
@@ -1,13 +1,13 @@
-import Student from "./components/Student"
+import Student from "./components/Student.jsx"
 
 function App() {
 
   return (
      <>
-      <Student name="John" age="20" grade="A"/>
-      <Student name="Jane" age="22" grade="B"/>
-      <Student name="Bob" age="19" grade="C"/>
-      <Student name="Alice" age="21" grade="A"  topper={false}/>
+      <Student name="John" age={20} grade="A"/>
+      <Student name="Jane" age={22} grade="B"/>
+      <Student name="Bob" age={19} grade="C"/>
+      <Student name="Alice" age={21} grade="A" />
     </>
      
   )
--- practice03/src/components/Student.jsx
+++ practice03/src/components/Student.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from "prop-types";
+
 
 const Student = (props) => {
   return (
@@ -7,18 +9,22 @@ const Student = (props) => {
       <p className="age-class">Age: {props.age}</p>
       <p className="grade-class">Student Grade: {props.grade}</p>
       <p className="hostel-class">
-       Hostel: {props.age >= 21
+        Campus Placement:{" "}
+        {props.age >= 21
           ? "Eligible for campus placement"
           : "Not eligible for campus placement"}
       </p>
       <p>
-        <p>
-            Is A Topper : {props.grade === "A" ? "Good Student" : "Average Student"}
-        </p>
-
+        Is A Topper : {props.grade === "A" ? "Good Student" : "Average Student"}
       </p>
     </div>
   );
+};
+
+Student.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number,
+  grade: PropTypes.string,
 };
 
 export default Student;
